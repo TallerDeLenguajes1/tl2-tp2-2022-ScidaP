40da1e8506698b19092426babb6ff5db62a542de
Patricio Scida <patricioscida@gmail.com>
2022-09-09 19:31:14 -0300

    agregado OOP

---
+++ Alumno.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2 {
+    class Alumno {
+        int id;
+        string nombre;
+        string apellido;
+        int dni;
+        string curso;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Dni { get => dni; set => dni = value; }
+        public string Curso { get => curso; set => curso = value; }
+
+        public void mostrarDatos() {
+            Console.WriteLine("Nombre: " + this.Nombre);
+            Console.WriteLine("Apellido: " + this.Apellido);
+            Console.WriteLine("DNI: " + this.Dni);
+            Console.WriteLine("ID: " + this.Id);
+            Console.WriteLine("Curso: " + NombreCurso(this.curso));
+        }
+
+        private static string NombreCurso(string CodigoCurso) {
+            string NombreCompleto;
+            switch (CodigoCurso) {
+                case "A":
+                    NombreCompleto = "Atletismo";
+                    break;
+                case "V":
+                    NombreCompleto = "Voley";
+                    break;
+                case "F":
+                    NombreCompleto = "Futbol";
+                    break;
+                default: NombreCompleto = "Error";
+                    break;
+            }
+            return NombreCompleto;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,8 +3,25 @@ using NLog;
 
 namespace TP2 {
     class Program {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ingrese la cantidad de alumnos a agregar");
+            int cantidad = Convert.ToInt32(Console.ReadLine());
+            while (cantidad != 0) {
+                Alumno nuevoAlumno = new Alumno();
+                Console.WriteLine("Nombre: ");
+                nuevoAlumno.Nombre = Console.ReadLine();
+                Console.WriteLine("Apellido: ");
+                nuevoAlumno.Apellido = Console.ReadLine();
+                Console.WriteLine("DNI: ");
+                nuevoAlumno.Dni = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Id: ");
+                nuevoAlumno.Id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Curso (A/V/F): ");
+                nuevoAlumno.Apellido = Console.ReadLine();
+                nuevoAlumno.mostrarDatos();
+                cantidad--;
+            }
         }
     }
 }

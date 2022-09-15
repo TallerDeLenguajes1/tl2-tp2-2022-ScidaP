8e4902c58778861db790bc36fb748f9e54636ee0
Patricio Scidá <patricioscida@gmail.com>
2022-09-14 21:07:47 -0300

    csvhelper crea el archivo pero no lo escribe

--- Program.cs
+++ Program.cs
@@ -1,27 +1,53 @@
 ﻿using System;
 using NLog;
 using CsvHelper;
+using System.IO;
+using System.Globalization;
+using System.Collections.Generic;
 
 namespace TP2 {
     class Program {
         private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
         static void Main(string[] args) {
+            CargarAlumnos();
+        }
+
+        static public void CargarAlumnos() {
+            List<Alumno> ListaAlumnos = new List<Alumno>();
             Console.WriteLine("Ingrese la cantidad de alumnos a agregar");
-            int cantidad = Convert.ToInt32(Console.ReadLine());
-            while (cantidad != 0) {
-                Console.WriteLine("Nombre: ");
-                string Nombre = Console.ReadLine();
-                Console.WriteLine("Apellido: ");
-                string Apellido = Console.ReadLine();
-                Console.WriteLine("DNI: ");
-                int Dni = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Id: ");
-                int Id = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Curso (A/V/F): ");
-                string Curso = Console.ReadLine();
-                Alumno nuevoAlumno = new(Id, Nombre, Apellido, Dni, Curso);
-                nuevoAlumno.mostrarDatos();
-                cantidad--;
+            try {
+                int cantidad = Convert.ToInt32(Console.ReadLine());
+                while (cantidad != 0) {
+                    Console.WriteLine("Nombre: ");
+                    string Nombre = Console.ReadLine();
+                    Console.WriteLine("Apellido: ");
+                    string Apellido = Console.ReadLine();
+                    Console.WriteLine("DNI: ");
+                    int Dni = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Id: ");
+                    int Id = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Curso (A/V/F): ");
+                    string Curso = Console.ReadLine();
+                    Alumno nuevoAlumno = new(Id, Nombre, Apellido, Dni, Curso);
+                    nuevoAlumno.mostrarDatos();
+                    ListaAlumnos.Add(nuevoAlumno);
+                    cantidad--;
+                }
+            } catch (Exception e) {
+                logger.Error("Error: " + e.Message);
+                Console.WriteLine("Error: detalles en el log");
+            }
+            guardarEnCsv(ListaAlumnos);
+        }
+
+        static public void guardarEnCsv(List<Alumno> ListaAlumno) {
+            try {
+                var stream = new FileStream("alumnos.csv", FileMode.OpenOrCreate);
+                var writer = new StreamWriter(stream);
+                var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
+                csv.WriteRecords(ListaAlumno);
+            } catch (Exception e) {
+                logger.Error("Error: " + e.Message);
             }
         }
     }
